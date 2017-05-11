be5f0df95752f5a89c0f9d95169256e1590ed525
vanita22 <inesvanessapcm@gmail.com>
2017-05-11 14:00:22 -0300

    subiendo otra vez

---
+++ java.js
@@ -0,0 +1,31 @@
+document.getElementById('toggle').addEventListener('click', function(event) {
+    event.preventDefault();
+    document.getElementById('nav-header').classList.toggle('open');
+    document.getElementById('body').classList.toggle('overflow-hidden');
+});
+
+var currentScroll = window.pageYOffset || document.body.scrollTop;
+var lastScrollTop = 0;
+
+window.addEventListener('scroll', function() {
+    var currentScroll = window.pageYOffset || document.body.scrollTop;
+    if (currentScroll > lastScrollTop) {
+        document.getElementById('nav-background').classList.remove('header-in');
+        document.getElementById('nav-background').style.opacity = 0;
+    } else {
+        document.getElementById('nav-header').classList.add('header-in');
+        document.getElementById('nav-header').classList.add('solid');
+        document.getElementById('nav-header').style.opacity = 1;
+        if (currentScroll <= 3) {
+            document.getElementById('nav-header').classList.remove('solid');
+            document.getElementById('nav-header').classList.remove('header-in');
+        }
+    }
+    lastScrollTop = currentScroll;
+}, false);
+
+document.getElementById('toggle').addEventListener('click', function(event) {
+    event.preventDefault();
+    document.getElementById('header').classList.toggle('open');
+    document.getElementById('body').classList.toggle('overflow-hidden');
+});
